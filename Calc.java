9532a09b428cdcc58a079beb37f4709b86409fa4
viveksahu06 <viveksahu1006@gmail.com>
2025-01-07 10:01:17 +0530

    changes done

--- Calc.java
+++ Calc.java
@@ -3,10 +3,10 @@ public class Calc{
    public static void main(String []args){ 
           Scanner sc=new Scanner(System.in); 
           System.out.println("Welcome to calculator"); 
-          System.out.print("Enter your first no "); 
-           int a=sc.nextInt(); 
-          System.out.print("Enter your second no "); 
-         int b=sc.nextInt();  
+          System.out.print("Enter your first no : "); 
+          int a=sc.nextInt(); 
+          System.out.print("Enter your second no : "); 
+          int b=sc.nextInt();  
           
          System.out.print("Enter Operation "); 
          char ope=sc.next().charAt(0); 
@@ -14,7 +14,7 @@ public class Calc{
          else if(ope=='-')   System.out.println(a-b); 
          else if(ope=='*')   System.out.println(a*b);  
          else if(ope=='/' && b>0)   System.out.println((float)a/b); 
-   
+         else if(ope=='/' && b==0) System.out.println("Number cannot be divide by zero");
         else  System.out.println("Incorrect"); 
     }
 }
