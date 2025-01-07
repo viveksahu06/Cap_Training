b87d4d396a9f2eb0195613db0c7d4360ed284944
viveksahu06 <viveksahu1006@gmail.com>
2025-01-07 10:05:32 +0530

    some changes

--- Calc.java
+++ Calc.java
@@ -2,19 +2,20 @@ import java.util.Scanner;
 public class Calc{ 
    public static void main(String []args){ 
           Scanner sc=new Scanner(System.in); 
-          System.out.println("Welcome to calculator"); 
-          System.out.print("Enter your first no "); 
-           int a=sc.nextInt(); 
-          System.out.print("Enter your second no "); 
+          System.out.println("*******Welcome to calculator*********"); 
+          System.out.print("Enter your first no: "); 
+          int a=sc.nextInt(); 
+          System.out.print("Enter your second no: "); 
          int b=sc.nextInt();  
           
-         System.out.print("Enter Operation "); 
-         char ope=sc.next().charAt(0); 
+         System.out.print("Enter Operation: "); 
+         char ope=sc.next().charAt(0);  
+         System.out.print("Answer is : ")
          if(ope=='+')   System.out.println(a+b); 
          else if(ope=='-')   System.out.println(a-b); 
          else if(ope=='*')   System.out.println(a*b);  
          else if(ope=='/' && b>0)   System.out.println((float)a/b); 
    
-        else  System.out.println("Incorrect"); 
+         else  System.out.println("Incorrect"); 
     }
 }
