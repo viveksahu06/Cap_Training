842df88017537f23633b5e75e79a2feced7a1753
viveksahu06 <viveksahu1006@gmail.com>
2025-01-07 12:26:10 +0530

    digit sum

--- Calc_BACKUP_1851.java
+++ Calc_BACKUP_1851.java
@@ -2,19 +2,32 @@ import java.util.Scanner;
 public class Calc{ 
    public static void main(String []args){ 
           Scanner sc=new Scanner(System.in); 
-          System.out.println("*****Welcome to calculator*****"); 
+<<<<<<< HEAD
+          System.out.println("*****Welcome to calculator****"); 
           System.out.print("Enter your first no : "); 
           int a=sc.nextInt(); 
           System.out.print("Enter your second no : "); 
           int b=sc.nextInt();  
+=======
+          System.out.println("*******Welcome to calculator*********"); 
+          System.out.print("Enter your first no: "); 
+          int a=sc.nextInt(); 
+          System.out.print("Enter your second no: "); 
+         int b=sc.nextInt();  
+>>>>>>> second
           
-         System.out.print("Enter Operation "); 
-         char ope=sc.next().charAt(0); 
+         System.out.print("Enter Operation: "); 
+         char ope=sc.next().charAt(0);  
+         System.out.print("Answer is : ")
          if(ope=='+')   System.out.println(a+b); 
          else if(ope=='-')   System.out.println(a-b); 
          else if(ope=='*')   System.out.println(a*b);  
          else if(ope=='/' && b>0)   System.out.println((float)a/b); 
+<<<<<<< HEAD
          else if(ope=='/' && b==0) System.out.println("Number cannot be divide by zero");
+=======
+   
+>>>>>>> second
          else  System.out.println("Incorrect"); 
     }
 }
--- Calc_LOCAL_1851.java
+++ Calc_LOCAL_1851.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 public class Calc{ 
    public static void main(String []args){ 
           Scanner sc=new Scanner(System.in); 
-          System.out.println("*****Welcome to calculator*****"); 
+          System.out.println("*****Welcome to calculator****"); 
           System.out.print("Enter your first no : "); 
           int a=sc.nextInt(); 
           System.out.print("Enter your second no : "); 
